f39656ffccbde474c18c6d25b750137239b2dcc5
Paul Colomiets <paul@colomiets.name>
2015-08-29 00:02:49 +0300

    Add some crate-levle docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,24 @@
+//! Signal handling library
+//!
+//! The library is focused on higher-level abstractions for handling signals.
+//! All low-level stuff should be in `nix`.
+//!
+//! Currently we have two mechanisms for handling exeptions:
+//!
+//! 1. The `exec_handler` module for replacing process with newly runned
+//!    command designed as crash safety measure
+//! 2. The `Trap` mechanism that masks out signals and allows wait for them
+//!    explicitly
+//!
+//! Both are specifically suited for making process supervisors.
+//!
+//! On TODO list:
+//!
+//! * `signalfd`
+//!
+//! The library tested only on linux
+//!
+
 extern crate libc;
 extern crate nix;
 
